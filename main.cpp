f3b2ce0d6c8da97fcb17a62697402deaa5828f9a
Student <Student@202-9>
2019-07-03 20:17:11 +0300

    Functions called

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ void Print(int Arr[], const int n);
 void main()
 {
 	setlocale(LC_ALL, "");
+	const int n = 5;
+	int Arr[n];
+	FillRand(Arr, n);
+	Print(Arr, n);
 }
 
 void FillRand(int Arr[], const int n)
